51ac8222a3dce2df7116d2536f2a9faf42778c72
Silwed <waltyahoo77@gmail.com>
2022-09-13 21:19:55 -0600

    Tipos de datos en js

---
+++ JavaScript/Leccion02/02-02-tipos-de-datos.js
@@ -0,0 +1,21 @@
+/*
+    Ejemplo de tipo de Datos en JavaScript
+*/
+
+//Ejemplo de tipo de dato String
+var cadena = "Prueba string";
+console.log(cadena);
+
+//Ejemplo de tipo de dato Numerico
+var numero = 1000;
+console.log(numero);
+
+//Ejemplo de tipo de dato Objeto
+var objeto = {
+    nombre :   "Juan",
+    apellido : "Moz",
+    telefono :  "456513218"
+
+}
+
+console.log(objeto);
