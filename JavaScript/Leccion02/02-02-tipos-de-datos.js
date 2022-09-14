6fb1fd515436badd416dc913bc94561543b21eba
Silwed <waltyahoo77@gmail.com>
2022-09-13 21:48:54 -0600

    actualizacion de leccion 2

--- JavaScript/Leccion02/02-02-tipos-de-datos.js
+++ JavaScript/Leccion02/02-02-tipos-de-datos.js
@@ -6,8 +6,18 @@
 var cadena = "Prueba string";
 console.log(cadena);
 
+//Ejemplo que la varibles se puede cambiar de atributo de foma dinamica
+cadena = 10;
+console.log(cadena);
+//para saber que tipo de dato es la variable
+console.log(typeof cadena);
+
+
 //Ejemplo de tipo de dato Numerico
 var numero = 1000;
+console.log(numero);
+//los float se manejan con el mismo tipo
+var numero = 10.55;
 console.log(numero);
 
 //Ejemplo de tipo de dato Objeto
