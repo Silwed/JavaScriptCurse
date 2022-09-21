9a9bc981343ce80f3d9acfb0bd85666d59d2344a
Silwed <waltyahoo77@gmail.com>
2022-09-20 21:36:39 -0600

    operadores y sintaxis de var

---
+++ JavaScript/Leccion02/02-04-Syntaxis-de-declaracion.js
@@ -0,0 +1,28 @@
+//Declaracion de syntaxis de java 
+//en la acualidad se usa lett para una variable
+//y const para una constante
+
+//esta forma es valido pero no es buena practica
+nombre = "Juan Carlos";
+console.log( nombre );
+
+//esta es la forma correcta;
+let nombre2 = "Juan Carlos";
+console.log( nombre2 );
+//----------------------------------
+let nombre3 ;
+nombre3 = "Vanessa Alfaro";
+console.log( nombre3 );
+
+//utilizando const
+const xs = "algo";
+xs = "asda";
+
+//formas de nombrar variables/metodos
+let a1nombreCompleto;
+let _nombreCompelto;
+let $nombreCompleto;
+//let 1nombreCompleto; no esta permitido el iniciar
+//el nombre de una variable con un numero o caracteres especiales que no sean _ y $
+
+
---
+++ JavaScript/Leccion03/03-01-Operadores_JavaScript.js
@@ -0,0 +1,26 @@
+let a = 3, b = 2;
+let z;
+
+//operador suma " + "
+z = a + b;
+console.log( "Resultado de la suma: " + z );
+
+//Operador resta " - "
+z = a - b;
+console.log( "Resultado de la resta: " + z );
+
+//Operados multiplicacion " * "
+z = a * b;
+console.log( "Resultado de la multiplicacion: " + z );
+
+//Operados divicion " / "
+z= a / b;
+console.log( "Resultado de la divicion: " + z );
+
+//Operados residuo de divicion " % "
+z = a % b;
+console.log( "Resultado del residuo: " + z);
+
+//Operador exponente (eleva un numero a un exponente)
+z = a ** b;
+console.log( "Resultado del exponente: " + z );
