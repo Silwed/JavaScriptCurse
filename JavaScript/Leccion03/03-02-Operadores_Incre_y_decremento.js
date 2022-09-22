e44ea6194cbd8c7d83492789337ec08519e36c63
Silwed <waltyahoo77@gmail.com>
2022-09-21 16:57:16 -1200

    Operadores Incremento y decremento

---
+++ JavaScript/Leccion03/03-02-Operadores_Incre_y_decremento.js
@@ -0,0 +1,23 @@
+let a = 3, b = 2;
+let z = a + b;
+
+//Leccion incremento
+//Ejeplo de pre-incremento (el ++ antes de la variable)
+z = ++a;
+console.log( a );
+console.log( z );
+
+//Ejeplo de post-incremento (el ++ despues de la variable)
+z = b++;
+console.log( b );
+console.log( z );
+
+//Ejemplo de pre-decremento (el -- antes de la variable)
+z = --a;
+console.log( a );
+console.log( z );
+
+//Ejemplo de post-decremento (el -- despues de la variable)
+z = b--;
+console.log( b );
+console.log( z );
